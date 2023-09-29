583e16eb86d775901d35d5e28b9b24e799c871ee
Ubuntu <ubuntu@ip-172-31-38-155.ec2.internal>
2023-09-29 22:34:00 +0000

    modular image classification code ready

---
+++ app-tier/driver.py
@@ -0,0 +1,14 @@
+from image_classification import Classifier
+
+
+if __name__ == "__main__":
+    # Create an instance of the Classifier class
+    classifier = Classifier()
+    
+    # Call the get_classification method with a URL
+    # url = f"/home/ubuntu/image_proc_folder/test_{i}.JPEG"
+
+    for i in range(10):
+        url =  f"/home/ubuntu/image_proc_folder/test_{i}.JPEG"
+        classification = classifier.get_classification(url)
+        print(f"{url}: {classification}")
