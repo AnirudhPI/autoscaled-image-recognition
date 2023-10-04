62922d83113bd0c3b34b9649f18321aa565fceba
hasagar97 <hrishikeshsagar8@gmail.com>
2023-10-03 18:20:53 -0700

    Added error handling

--- app-tier/driver.py
+++ app-tier/driver.py
@@ -14,32 +14,35 @@ s3_results = S3FileManager("cumulonimbus-clouds-results")
 classifier = Classifier()
 
 while True:
-    
-    if input_sqs_queue.get_queue_length() > 0:
-        # Receive messages from the input SQS queue
-        messages = input_sqs_queue.receive_messages(max_messages=1)
+    try:     
+        if input_sqs_queue.get_queue_length() > 0:
+            # Receive messages from the input SQS queue
+            messages = input_sqs_queue.receive_messages(max_messages=1)
 
-        # Process the first message
-        message = messages[0]
-        s3_key = message['Body']
-        print(f"found message with key {s3_key}", end=" -->")
-        # Download the image from S3
-        image_path = f"/tmp/{s3_key}"
-        s3_images.copy_image_to_file(s3_key, image_path)
+            # Process the first message
+            message = messages[0]
+            s3_key = message['Body']
+            print(f"found message with key {s3_key}", end=" -->")
+            # Download the image from S3
+            image_path = f"/tmp/{s3_key}"
+            s3_images.copy_image_to_file(s3_key, image_path)
 
-        # Perform image classification (you need to implement this function)
-        classification_result =  classifier.get_classification(image_path)
+            # Perform image classification (you need to implement this function)
+            classification_result =  classifier.get_classification(image_path)
 
-        # Store the classification result in another S3 bucket
-        result_s3_key = s3_key
-        s3_results.upload_text(result_s3_key, classification_result)
+            # Store the classification result in another S3 bucket
+            result_s3_key = s3_key
+            s3_results.upload_text(result_s3_key, classification_result)
 
-        print(f"classification result {classification_result}")
-        # Delete the message from the input SQS queue
-        input_sqs_queue.delete_message(message['ReceiptHandle'])
+            print(f"classification result {classification_result}")
+            # Delete the message from the input SQS queue
+            input_sqs_queue.delete_message(message['ReceiptHandle'])
 
-        # Send a new message with the key and classification result to another SQS queue
-        output_sqs_queue.send_message(f"{s3_key}#$@$#{classification_result}")
-
-    # Sleep for a while before checking for new messages again
-    time.sleep(5)
+            # Send a new message with the key and classification result to another SQS queue
+            output_sqs_queue.send_message(f"{s3_key}#$@$#{classification_result}")
+        else:
+            time.sleep(5)
+    except:
+        print("[Warning] the SQS queue depth is not updated, expected behavior due to processing lags")
+        # Sleep for a while before checking for new messages again
+        time.sleep(5)
